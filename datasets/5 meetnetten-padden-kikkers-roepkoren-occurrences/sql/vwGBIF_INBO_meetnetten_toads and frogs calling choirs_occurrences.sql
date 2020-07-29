c2bdea439a38c3b641dc3911e6493773bf214b9b
DimEvil <dimibrosens@gmail.comm>
2020-07-29 14:07:38 +0200

    add choir queries

--- datasets/5 meetnetten-padden-kikkers-roepkoren-occurrences/sql/vwGBIF_INBO_meetnetten_toads and frogs calling choirs_occurrences.sql
+++ datasets/5 meetnetten-padden-kikkers-roepkoren-occurrences/sql/vwGBIF_INBO_meetnetten_toads and frogs calling choirs_occurrences.sql
@@ -1,7 +1,7 @@
 USE [S0008_00_Meetnetten]
 GO
 
-/****** Object:  View [iptdev].[vwGBIF_INBO_meetnetten_toads and frogs calling choirs_occurrence]    Script Date: 3/09/2019 15:31:12 ******/
+/****** Object:  View [iptdev].[vwGBIF_INBO_meetnetten_05_33_amfibieen_roepkoor_occurrences]    Script Date: 29/07/2020 14:06:59 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -12,22 +12,16 @@ GO
 
 
 
-
-
-
-
-
-
 /* Generieke query inclusief soorten */
 
 
-ALTER VIEW [iptdev].[vwGBIF_INBO_meetnetten_toads and frogs calling choirs_occurrence]
+CREATE VIEW [ipt].[vwGBIF_INBO_meetnetten_05_33_amfibieen_roepkoor_occurrences]
 AS
 
 SELECT --fa.*   --unieke kolomnamen
 	
 
-	 [occurrenceID] = N'INBO:MEETNETTEN:OCC:' + Right( N'0000' + CONVERT(nvarchar(20) ,FieldworkObservationID),7)
+	 [occurrenceID] = N'INBO:MEETNET:OCC:' + Right( N'0000' + CONVERT(nvarchar(20) ,FieldworkObservationID),7)
 
 	---RECORD ---
 
@@ -37,18 +31,26 @@ SELECT --fa.*   --unieke kolomnamen
 	, [rightsHolder] = N'INBO'
 	, [accessRights] = N'https://www.inbo.be/en/norms-data-use'
 	, [datasetID] = N'meetnettendatasetDOI'
-	, [datasetName] = N'Meetnetten - Generiek Vuursalamander, Belgium'
+	, [datasetName] = N'Meetnetten - vlinders transecten, Belgium'
 	, [institutionCode] = N'INBO'
 
 	**/
 	
 	 ---EVENT---	
 	
-	,  [eventID ] = N'INBO:MEETNET:EVENT:' + Right( N'000000000' + CONVERT(nvarchar(20) , fA.FieldworkSampleID),6)  
+	, [eventID ] = N'INBO:MEETNET:EVENT:' + Right( N'000000000' + CONVERT(nvarchar(20) , fA.FieldworkSampleID),6)  
 	, [basisOfRecord] = N'HumanObservation'
-	, [samplingProtocol] = Protocolname
-	, [lifeStage] = SpeciesLifestageName
-	, [protocol] = ProtocolSubjectDescription
+--	, [samplingProtocol] = Protocolname
+	, [lifeStage] = CASE SpeciesLifestageName
+					WHEN 'exuvium' THEN 'exuviae'
+					WHEN 'imago (not fully colored)' THEN 'imago'
+					ELSE SpeciesLifestageName
+					END
+	, [occurrenceStatus] = case
+						  when Aantal > '0' then 'present'
+						  Else 'absent'
+						  END
+--	, [protocol] = ProtocolSubjectDescription
 	
 --	, [samplingEffort] =
 						
@@ -58,41 +60,52 @@ SELECT --fa.*   --unieke kolomnamen
 
 	--, CONVERT(decimal(10,5), dL.LocationGeom.STCentroid().STY) as decimalLatitude
 	--, CONVERT(decimal(10,5), dL.LocationGeom.STCentroid().STX) as decimalLongitude
-/**
-	---LOCATION
-	, [locationID] = N'INBO:MEETNET:LOCATION:' + Right( N'000000000' + CONVERT(nvarchar(20) ,dL.LocationID),10) 
-	, [continent] = N'Europe'
---	, [waterbody] = dL.Location
-	, [countryCode] = N'BE'
-	, [locality] = locationName
-	, [georeferenceRemarks] = N'coordinates are centroid of location' 
-	
-	, CONVERT(decimal(10,5), dL.LocationGeom.MakeValid().STCentroid().STY) as decimalLatitude
-	, CONVERT(decimal(10,5), dL.LocationGeom.MakeValid().STCentroid().STX) as decimalLongitude
-	, [geodeticDatum] = N'WGS84'
 
-	**/
+	---LOCATION
+--	, [locationID] = N'INBO:MEETNET:LOCATION:' + Right( N'000000000' + CONVERT(nvarchar(20) ,dL.LocationID),10) 
+--	, [continent] = N'Europe'
+--	, [waterbody] = dL.Location
+--	, [countryCode] = N'BE'
+--	, [locality] = locationName
+--	, [georeferenceRemarks] = N'coordinates are centroid of location' 
+	
+--	, CONVERT(decimal(10,5), dL.LocationGeom.MakeValid().STCentroid().STY) as decimalLatitude
+--	, CONVERT(decimal(10,5), dL.LocationGeom.MakeValid().STCentroid().STX) as decimalLongitude
+--	, [geodeticDatum] = N'WGS84'
+
+	
 		
 	---- OCCURRENCE ---
 		
-	, [recordedBy] = 'to complete'
+	, [recordedBy] = 'https://meetnetten.be'
 	, [individualCount] = Aantal
-
-	
-	
-	
+	, [sex] = CASE Geslacht
+				WHEN 'U' THEN 'unknown'
+				WHEN 'M' THEN 'male'
+				WHEN 'F' THEN 'female'
+				ELSE Geslacht
+				END
+	, [behaviour] = SpeciesActivityName
+		
 	----Taxon
 
 	, [scientificName] = SpeciesScientificName
 	, [vernacularName] = SpeciesName
-
 	, [kingdom] = N'Animalia'
 	, [phylum] = N'Chordata'
-	, [class] = N''
+	, [class] = N'Amphibia'
+--	, [order] = N''
 	, [nomenclaturalCode] = N'ICZN'
+	, [taxonRank] =	 case  SpeciesScientificName
+						  when  'Pieris spec.' THEN  N'genus'
+						  Else 'species'
+						  END
 	
-	, fa.ProjectKey
+--	, fa.ProjectKey
+--	, [occurrenceRemarks] = 'data collected in the '  + Dbl.ProjectName + ' monitoring scheme'
 
+
+	
 FROM dbo.FactAantal fA
 	INNER JOIN dbo.dimProject dP ON dP.ProjectKey = fA.ProjectKey
 	INNER JOIN dbo.DimLocation dL ON dL.LocationKey = fA.LocationKey
@@ -100,12 +113,30 @@ FROM dbo.FactAantal fA
 	INNER JOIN dbo.DimSpeciesActivity dSA ON dSA.SpeciesActivityKey = fA.SpeciesActivityKey
 	INNER JOIN dbo.DimSpeciesLifestage dSL ON dSL.SpeciesLifestageKey = fA.SpeciesLifestageKey
 	INNER JOIN dbo.DimSpecies dSP ON dsp.SpeciesKey = fa.SpeciesKey
+	INNER JOIN dbo.DimBlur Dbl ON Dbl.ProjectKey = fa.projectKey
+	INNER JOIN (SELECT DISTINCT(FieldworkSampleID), VisitStartDate FROM dbo.FactWerkpakket ) FWp ON FWp.FieldworkSampleID = fa.FieldworkSampleID
 
 	--INNER JOIN FactCovariabele FCo ON FCo.FieldworkSampleID = fA.FieldworkSampleID
 WHERE 1=1
----AND ProjectName = 'Vuursalamander'
+--AND ProjectName = '***'
 --AND fa.ProjectKey = '16'
-AND fa.ProtocolID =  '5'
+AND fa.ProtocolID IN ('5','33')  ---Amphibia choirs *
+AND fwp.VisitStartDate > CONVERT(datetime, '2016-01-01', 120)
+AND fwp.VisitStartDate < CONVERT(datetime, '2019-12-31', 120)
+
+--AND SpeciesScientificName like 'Pieris spec.'
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
