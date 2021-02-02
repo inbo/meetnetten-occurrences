d2b51ebb3d23edc5cd25d97c7b9e8b0bea8c8dde
DimEvil <dimibrosens@gmail.comm>
2021-02-02 16:17:56 +0100

    new queries

--- datasets/29 meetnetten-dagvlinders-inbo-occurrences/sql/vwGBIF_INBO_meetnetten_butterfly transects(algemene monitoring)_occurrences.sql
+++ datasets/29 meetnetten-dagvlinders-inbo-occurrences/sql/vwGBIF_INBO_meetnetten_butterfly transects(algemene monitoring)_occurrences.sql
@@ -1,7 +1,7 @@
 USE [S0008_00_Meetnetten]
 GO
 
-/****** Object:  View [iptdev].[vwGBIF_INBO_meetnetten_butterfly transects(algemene monitoring)_occurrence]    Script Date: 3/09/2019 15:28:27 ******/
+/****** Object:  View [ipt].[vwGBIF_INBO_meetnetten_29_vlinders_transecten_alg_occurrences]    Script Date: 2/02/2021 14:17:00 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -11,23 +11,16 @@ GO
 
 
 
-
-
-
-
-
-
-
 /* Generieke query inclusief soorten */
 
 
-ALTER VIEW [iptdev].[vwGBIF_INBO_meetnetten_butterfly transects(algemene monitoring)_occurrence]
+ALTER VIEW [ipt].[vwGBIF_INBO_meetnetten_29_vlinders_transecten_alg_occurrences]
 AS
 
 SELECT --fa.*   --unieke kolomnamen
 	
 
-	 [occurrenceID] = N'INBO:MEETNETTEN:OCC:' + Right( N'0000' + CONVERT(nvarchar(20) ,FieldworkObservationID),7)
+	 [occurrenceID] = N'INBO:MEETNET:OCC:' + Right( N'0000' + CONVERT(nvarchar(20) ,FieldworkObservationID),7)
 
 	---RECORD ---
 
@@ -37,18 +30,36 @@ SELECT --fa.*   --unieke kolomnamen
 	, [rightsHolder] = N'INBO'
 	, [accessRights] = N'https://www.inbo.be/en/norms-data-use'
 	, [datasetID] = N'meetnettendatasetDOI'
-	, [datasetName] = N'Meetnetten - Butterfly transects (algemene monitoring)'
+	, [datasetName] = N'Meetnetten - vlinders transecten, Belgium'
 	, [institutionCode] = N'INBO'
 
 	**/
 	
 	 ---EVENT---	
 	
-	,  [eventID ] = N'INBO:MEETNET:EVENT:' + Right( N'000000000' + CONVERT(nvarchar(20) , fA.FieldworkSampleID),6)  
+	, [eventID ] = N'INBO:MEETNET:EVENT:' + Right( N'000000000' + CONVERT(nvarchar(20) , fA.FieldworkSampleID),6)  
 	, [basisOfRecord] = N'HumanObservation'
-	, [samplingProtocol] = Protocolname
-	, [lifeStage] = SpeciesLifestageName
-	, [protocol] = ProtocolSubjectDescription
+	, [samplingProtocol] =  case Protocolname
+							WHEN 'Vlinders - Transecten (algemene monitoring)' THEN 'Flemish butterfly monitoring scheme'
+							ELSE ' '
+							END
+	
+	, [occurrenceStatus] = case
+						  when Aantal > '0' then 'present'
+						  Else 'absent'
+						  END
+	--, [occurrenceRemarks] = case 
+	--					  when SpeciesScientificName IN ('Lasiommata megera') AND fa.ProjectKey = 40  then 'target species'
+	--					  when SpeciesScientificName IN ('Pyrgus malvae') AND fa.ProjectKey = 157  then 'target species'
+	--					  when SpeciesScientificName IN ('Melitaea cinxia') AND fa.ProjectKey = 25  then 'target species'
+	--					  when SpeciesScientificName IN ('Pyronia tithonus') AND fa.ProjectKey = 175  then 'target species'
+	--					  when SpeciesScientificName IN ('Hipparchia semele') AND fa.ProjectKey = 35  then 'target species'
+	--					  when SpeciesScientificName IN ('Erynnis tages') AND fa.ProjectKey = 162  then 'target species'
+	--					  when SpeciesScientificName IN ('Cyaniris semiargus') AND fa.ProjectKey = 167  then 'target species'
+	--					  when SpeciesScientificName IN ('Hesperia comma') AND fa.ProjectKey = 30  then 'target species'
+	--					  Else 'casual observation'
+	--					  END
+--	, [protocol] = ProtocolSubjectDescription
 	
 --	, [samplingEffort] =
 						
@@ -60,23 +71,25 @@ SELECT --fa.*   --unieke kolomnamen
 	--, CONVERT(decimal(10,5), dL.LocationGeom.STCentroid().STX) as decimalLongitude
 
 	---LOCATION
-	, [locationID] = N'INBO:MEETNET:LOCATION:' + Right( N'000000000' + CONVERT(nvarchar(20) ,dL.LocationID),10) 
-	, [continent] = N'Europe'
+--	, [locationID] = N'INBO:MEETNET:LOCATION:' + Right( N'000000000' + CONVERT(nvarchar(20) ,dL.LocationID),10) 
+--	, [continent] = N'Europe'
 --	, [waterbody] = dL.Location
-	, [countryCode] = N'BE'
-	, [locality] = locationName
-	, [georeferenceRemarks] = N'coordinates are centroid of location' 
+--	, [countryCode] = N'BE'
+--	, [locality] = locationName
+--	, [georeferenceRemarks] = N'coordinates are centroid of location' 
 	
-	, CONVERT(decimal(10,5), dL.LocationGeom.MakeValid().STCentroid().STY) as decimalLatitude
-	, CONVERT(decimal(10,5), dL.LocationGeom.MakeValid().STCentroid().STX) as decimalLongitude
-	, [geodeticDatum] = N'WGS84'
+--	, CONVERT(decimal(10,5), dL.LocationGeom.MakeValid().STCentroid().STY) as decimalLatitude
+--	, CONVERT(decimal(10,5), dL.LocationGeom.MakeValid().STCentroid().STX) as decimalLongitude
+--	, [geodeticDatum] = N'WGS84'
 
 	
 		
 	---- OCCURRENCE ---
 		
-	, [recordedBy] = 'to complete'
+	, [recordedBy] = 'https://meetnetten.be'
 	, [individualCount] = Aantal
+	, [lifeStage] = SpeciesLifestageName
+	
 
 	
 	
@@ -85,14 +98,20 @@ SELECT --fa.*   --unieke kolomnamen
 
 	, [scientificName] = SpeciesScientificName
 	, [vernacularName] = SpeciesName
-
 	, [kingdom] = N'Animalia'
 	, [phylum] = N'Arthropoda'
-	, [class] = N''
+	, [class] = N'Insecta'
+	, [order] = N'Lepidoptera'
 	, [nomenclaturalCode] = N'ICZN'
+	, [taxonRank] =	 case  SpeciesScientificName
+						  when  'Pieris spec.' THEN  N'genus'
+						  Else  'species'
+						  END
 	
-	, fa.ProjectKey
+--	, fa.ProjectKey
+--	, [occurrenceRemarks] = 'data collected in the '  + Dbl.ProjectName + ' monitoring scheme'
 
+ 	
 FROM dbo.FactAantal fA
 	INNER JOIN dbo.dimProject dP ON dP.ProjectKey = fA.ProjectKey
 	INNER JOIN dbo.DimLocation dL ON dL.LocationKey = fA.LocationKey
@@ -100,12 +119,28 @@ FROM dbo.FactAantal fA
 	INNER JOIN dbo.DimSpeciesActivity dSA ON dSA.SpeciesActivityKey = fA.SpeciesActivityKey
 	INNER JOIN dbo.DimSpeciesLifestage dSL ON dSL.SpeciesLifestageKey = fA.SpeciesLifestageKey
 	INNER JOIN dbo.DimSpecies dSP ON dsp.SpeciesKey = fa.SpeciesKey
+	INNER JOIN dbo.DimBlur Dbl ON Dbl.ProjectKey = fa.projectKey
+	INNER JOIN (SELECT DISTINCT(FieldworkSampleID), VisitStartDate FROM dbo.FactWerkpakket ) FWp ON FWp.FieldworkSampleID = fa.FieldworkSampleID
 
 	--INNER JOIN FactCovariabele FCo ON FCo.FieldworkSampleID = fA.FieldworkSampleID
 WHERE 1=1
----AND ProjectName = 'Butterfly transects (algemene monitoring)'
+--AND ProjectName = '***'
 --AND fa.ProjectKey = '16'
-AND fa.ProtocolID =  '29'
+AND fa.ProtocolID IN ('29') ---Vlinders transecten algemeen
+--AND fwp.VisitStartDate > CONVERT(datetime, '2016-01-01', 120)
+AND fwp.VisitStartDate < CONVERT(datetime, '2020-12-31', 120)
+
+--AND SpeciesScientificName like 'Pieris spec.'
+--AND FieldworkObservationID in ('510509')
+
+--AND fwp.VisitStartDate = CONVERT(datetime, '1993-06-20', 120)
+--AND speciesScientificName = ('Celastrina argiolus')
+
+
+
+
+
+
 
 
 
